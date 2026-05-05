d8c7726f60b12888393731fdbc8fc99e1c6aa825
MadsDamiri87 <madsc.damiri@gmail.com>
2026-05-05 09:39:15 +0200

    done

--- src/MainServerUDP.java
+++ src/MainServerUDP.java
@@ -1,6 +1,10 @@
+import model.DTO.SensorReadingDTO;
 import server.ServerSocketManagerTCP;
 import server.ServerSocketManagerUDP;
 import service.BlindsService;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 
 // Bruges kun til isoleret test af UDP-serveren.
 // Det hele startes normalt via RunApp.
@@ -8,8 +12,7 @@ public class MainServerUDP
 {
   public static void main(String[] args)
   {
-    BlindsService blindsService = new BlindsService();
-    ServerSocketManagerTCP tcpManager = new ServerSocketManagerTCP(6790);
-    new ServerSocketManagerUDP(6789, blindsService, tcpManager);
+    BlockingQueue<SensorReadingDTO> queue = new ArrayBlockingQueue<>(20);
+    new ServerSocketManagerUDP(6789, queue);
   }
 }
